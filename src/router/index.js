3626c8f9186360dfc673ffe83fcc45d7d8403e71
hopzebordah <aalcocerpeters@gmail.com>
2020-05-01 20:24:56 -0500

    dynamically load route components

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import TheHomeView from '@/views/TheHomeView'
-import TheCategoryView from '@/views/TheCategoryView'
-import TheSingleView from '@/views/TheSingleView'
-import The404View from '@/views/The404View'
-
-// I think I am done with checkpoint6 already?
 
 Vue.use(VueRouter)
 
@@ -14,23 +8,23 @@ const routes = [
         path: '/',
         alias: '/home',
         name: 'Home',
-        component: TheHomeView
+        component: () => import('@/views/TheHomeView')
     },
     {
         path: '/category/:category',
         name: 'Category',
         props: true,
-        component: TheCategoryView
+        component: () => import('@/views/TheCategoryView')
     },
     {
         path: '/video/:id',
         name: 'Single',
-        component: TheSingleView
+        component: () => import('@/views/TheSingleView')
     },
     {
         path: '*',
         name: '404 Not Found',
-        component: The404View
+        component: () => import('@/views/The404View')
     }
 ]
 
